d25bec48d675371229365eec362e6f8759a34a5f
muthuraman0508 <muthuraman.s2022ecea@sece.ac.in>
2025-02-11 15:15:28 +0530

    Changed from CAN 1 to CAN 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
 #include <Arduino.h>
 #include <FlexCAN_T4.h>
 
-FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> can1;
+FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> can3;
 
 void setup()
 {
   Serial.begin(115200);
   delay(100);
-  can1.begin();
+  can3.begin();
   delay(100);
-  can1.setBaudRate(500000);
+  can3.setBaudRate(500000);
   delay(1000);
-  Serial.println("CAN1 Receiver Initialized");
+  Serial.println("CAN2 Receiver Initialized");
   delay(100);
 }
 
@@ -20,7 +20,7 @@ void loop()
   delay(100);
   CAN_message_t msg;
   delay(100);
-  if (can1.read(msg))
+  if (can3.read(msg))
   { // Check if message is received
     Serial.print("Received Message: ");
     for (int i = 0; i < msg.len; i++)
